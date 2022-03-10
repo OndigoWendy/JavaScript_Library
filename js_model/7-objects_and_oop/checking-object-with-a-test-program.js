1bdb59b9ae5291f52e643871baec58da2f39c1fd
OndigoWendy <ondigowendy004@gmail.com>
2022-03-10 23:21:27 +0300

    complete definition for the checking object along with a test program.

--- js_model/7-objects_and_oop/checking-object-with-a-test-program.js
+++ js_model/7-objects_and_oop/checking-object-with-a-test-program.js
@@ -12,5 +12,16 @@ function Checking(amount) {
     this.balance -= amount;
     }
     if (amount > this.balance) {
-    print("Insufficient funds");
+    console.log("Insufficient funds");
     }
+}
+function toString() {
+return "Balance: " + this.balance;
+}
+var account = new Checking(500);
+account.deposit(1000);
+console.log(account.toString()); // Balance: 1500
+account.withdraw(750);
+console.log(account.toString()); // Balance: 750
+account.withdraw(800); // displays "Insufficient funds"
+console.log(account.toString()); // Balance: 750
