e7123b472c802933bb1608bcae2b1cb2f9923127
OndigoWendy <ondigowendy004@gmail.com>
2022-03-15 00:04:21 +0300

    Array elements are also accessed using the [] operator.

--- arrays/0-using-arrays/access_array_elements.js
+++ arrays/0-using-arrays/access_array_elements.js
@@ -2,3 +2,8 @@ var nums = [];
 for (var i = 0; i < 100; ++i) {
 nums[i] = i+1;
 }
+
+var numbers = [1,2,3,4,5];
+var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] +
+numbers[4];
+print(sum); // displays 15
