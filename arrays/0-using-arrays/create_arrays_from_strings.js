22b384b9440a89990f7897431cd961478548344b
OndigoWendy <ondigowendy004@gmail.com>
2022-03-15 00:06:20 +0300

    program demonstrates how the split() function works on a simple string:

---
+++ arrays/0-using-arrays/create_arrays_from_strings.js
@@ -0,0 +1,8 @@
+var sentence = "the quick brown fox jumped over the lazy dog";
+var words = sentence.split(" ");
+for (var i = 0; i < words.length; ++i) {
+print("word " + i + ": " + words[i]);
+}
+copy(nums, samenums);
+nums[0] = 400;
+print(samenums[0]); // displays 1
