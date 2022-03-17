020447f62ebdc168fd36fa504cbfe07a22131ea4
OndigoWendy <ondigowendy004@gmail.com>
2022-03-18 00:09:48 +0300

    To perform a row-wise computation, we simply have to flip the for loops so that the outer loop controls the columns and the inner loop controls the rows

---
+++ arrays/4-two-dimensional-and-multidimensional-arrays/create-2d_array.js
@@ -0,0 +1,12 @@
+var twod = [];
+var rows = 5;
+for (var i = 0; i < rows; ++i) {
+twod[i] = [];
+
+
+average = total / grades[row].length;
+print("Student " + parseInt(row+1) + " average: " +
+average.toFixed(2));
+total = 0;
+average = 0.0;
+}
